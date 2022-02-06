77f04fbbb81d4eb975a3096148a546a40a4c07f3
chaquator <chaquator@gmail.com>
2022-02-05 18:09:13 -0600

    moved hud to public, small css change

--- src/hud/Hud.js
+++ src/hud/Hud.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import './Hud.css';
-
 function Hud() {
     const [showHud, setShowHud] = React.useState(null);
     const [redTeam, setRedTeam] = React.useState(null);
